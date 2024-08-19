40219dfb4e6beabe49de09309c95dd8a7eef08f0
airetOK <sergey.bezrukavyy@gmail.com>
2024-08-19 19:04:46 +0300

    Add frontend url to backend

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,17 @@
-from os import unlink
+import os
 from fastapi import FastAPI, BackgroundTasks
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
+from dotenv import load_dotenv
 
 from entity.system_design import SystemDesign
 from manager.pdf_manager import PdfManager
 
-import urllib.request
-
+load_dotenv()
 app = FastAPI()
-# TO:DO get URL from env
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=[os.getenv('FRONTEND_URL')],
     allow_credentials=False,
     allow_methods=["POST"],
     allow_headers=["*"],
